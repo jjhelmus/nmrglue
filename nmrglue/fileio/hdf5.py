bf261030cc2ae5ea19f7a0ad2818e27fb3029238
Jonathan Helmus <jjhelmus@gmail.com>
2010-03-01 18:30:53 +0000

    hdf5 module work

--- nmrglue/fileio/hdf5.py
+++ nmrglue/fileio/hdf5.py
@@ -83,9 +83,108 @@ def put_dic(f,dic,dataset="spectra"):
             dset.attrs[key] = value
     return
 
-class JDataset(h5py.Dataset):
-    """
-    Dataset subclass which behaves more like a Numpy ndarray
+
+
+class TDataset(object):
+    """ 
+    h5py object which supports transposes and swap axes methods
     """
 
-    pass
+    def __init__(self,Dataset,order=None):
+
+        self.Dataset = Dataset
+        
+        # set array like properties
+        self.dshape = Dataset.shape
+        self.dtype = Dataset.dtype 
+        self.ndim = len(Dataset.shape)
+
+        # order specifies how the axes match up to the Dataset axes
+        # self.or
+        if order == None:     # set order as normal if not specified
+            self.order = tuple(range(self.ndim))
+        else:
+            self.order = order
+
+        # shape from order and Dataset shape
+        self.shape = tuple([self.Dataset.shape[i] for i in self.order]) 
+
+
+    def __getitem__(self, args):
+        """ 
+        Read a slice from the HDF5 dataset.
+
+        Takes slices and recarray-style field names (more than one is
+        allowed!) in any order.  Obeys basic NumPy rules, including
+        broadcasting.
+
+        Also supports:
+
+        * Boolean "mask" array indexing
+        * Advanced dataspace selection via the "selections" module
+        """
+
+        args = args if isinstance(args, tuple) else (args,)
+
+        # Sort field indices from the rest of the args.
+        names = tuple(x for x in args if isinstance(x, str))
+        args = tuple(x for x in args if not isinstance(x, str))
+
+        # Create NumPy datatype for read, using only the named fields
+        # as specified by the user.
+        basetype = self.Dataset.dtype
+        if len(names) == 0:
+            new_dtype = basetype
+        elif basetype.names is None:
+            raise ValueError("Field names only allowed for compound types")
+        else:
+            for name in names:
+                if not name in basetype.names:
+                    raise ValueError("Field %s does not appear in this type." % name)
+                new_dtype = numpy.dtype([(name, basetype.fields[name][0]) for name in names])
+
+        # Perform the selection in the TDataset order.
+        sel = h5py.selections.select(self.shape, args)
+
+        if sel.nselect == 0:
+            return numpy.ndarray((0,), dtype=new_dtype)
+
+        if not isinstance(sel,h5py.selections.SimpleSelection):
+            raise TypeError("Only rectangular (regular) selections supported")
+
+        # unpack the selection
+        start,count,step,scalar = sel._sel
+        
+        # reorder these into Dataset order
+        dstart = tuple([start[i] for i in self.order])
+        dcount = tuple([count[i] for i in self.order])
+        dstep  = tuple([step[i] for i in self.order])
+        dscalar = tuple([scalar[i] for i in self.order])
+
+        # determind Dataset order mshape
+        dmshape = tuple(x for x, y in zip(dcount, dscalar) if not y)
+
+        # create a SpaceID object representing Dataset order selection
+        id = h5py.h5s.create_simple(self.Dataset.shape, 
+                    (h5py.h5s.UNLIMITED,)*len(self.Dataset.shape))
+        id.select_hyperslab(dstart,dcount,dstep)
+
+        # Create the output array to hold Dataset ordered selection
+        arr = np.ndarray(dmshape, new_dtype, order='C')
+
+        # This is necessary because in the case of array types, NumPy
+        # discards the array information at the top level.
+        mtype = h5py.h5t.py_create(new_dtype)
+
+        # Perfom the actual read
+        mspace = h5py.h5s.create_simple(dmshape)
+        fspace = id
+        self.Dataset.id.read(mspace, fspace, arr, mtype)
+
+        # swap axes to put into TDataset ordering
+        arr = arr.transpose(self.order)
+
+        # Patch up the output for NumPy
+        if arr.shape == ():
+            arr = np.asscalar(arr)
+        return arr
