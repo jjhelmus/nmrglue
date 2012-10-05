5b73715762a6254f93fdd09edc9382fee116fa55
Jonathan Helmus <jjhelmus@gmail.com>
2012-10-05 13:37:12 -0400

    STY: pep8 on misc.py

--- nmrglue/util/misc.py
+++ nmrglue/util/misc.py
@@ -5,12 +5,13 @@ Misc. functions
 import numpy as np
 
 # default tolerences
-rtol = 1.001e-01
-atol = 1.001e-01
-dtol = 1.001e-01
+RTOL = 1.001e-01
+ATOL = 1.001e-01
+DTOL = 1.001e-01
 
-def pair_similar(dic1, data1, dic2, data2, verb=False, atol=atol, rtol=rtol,
-    dtol=dtol):
+
+def pair_similar(dic1, data1, dic2, data2, verb=False, atol=ATOL, rtol=RTOL,
+    dtol=DTOL):
     """
     Check a dic, data pair against a second dic, data pair for differences.
 
@@ -37,16 +38,17 @@ def pair_similar(dic1, data1, dic2, data2, verb=False, atol=atol, rtol=rtol,
         True is data1 and data2 are similar, False if they differ.
     r2 : bool
         True is dic1 and dic2 are similar, False if they differ.
-    
+
     """
     r1 = isdatasimilar(data1, data2, verb, atol, rtol)
     r2 = isdicsimilar(dict(dic1), dict(dic2), verb, dtol)
     return r1, r2
 
-def isdatasimilar(data1, data2, verb=False, atol=atol, rtol=rtol):
-    """ 
+
+def isdatasimilar(data1, data2, verb=False, atol=ATOL, rtol=RTOL):
+    """
     Check that two sets of NMR data are equal within a tolerance.
-    
+
     Parameters
     ----------
     data1 : ndarray
@@ -64,7 +66,7 @@ def isdatasimilar(data1, data2, verb=False, atol=atol, rtol=rtol):
     -------
     r1 : bool
         True is data1 and data2 are similar, False if they differ.
-    
+
     """
     r = True
     if data1.dtype != data2.dtype:
@@ -81,12 +83,13 @@ def isdatasimilar(data1, data2, verb=False, atol=atol, rtol=rtol):
             print "Data does not match"
     return r
 
-def isitemsimilar(v1, v2, verb=False, dtol=dtol):
+
+def isitemsimilar(v1, v2, verb=False, dtol=DTOL):
     """
     Compare two values for differences
 
     See :py:func:`isdicsimilar` for Parameters.
-    
+
     """
 
     r = True
@@ -95,13 +98,12 @@ def isitemsimilar(v1, v2, verb=False, dtol=dtol):
     if type(v1) != type(v2):
         r = False
         if verb:
-            print "Key has different type", k, type(dic1[k]), \
-                    type(dic2[k])
-            
+            print "Item has different type", type(v1), type(v2)
+
     # iterable checking
     elif isinstance(v1, dict):
         r = r and isdicsimilar(v1, v2, verb=verb, dtol=dtol)
-        
+
     elif isinstance(v1, list):
         r = r and islistsimilar(v1, v2, verb=verb, dtol=dtol)
 
@@ -110,22 +112,23 @@ def isitemsimilar(v1, v2, verb=False, dtol=dtol):
         if abs(v1 - v2) > dtol:
             r = False
             if verb:
-                print "Key mismatch:", k, v1, v2
+                print "Key mismatch:", v1, v2
 
     # all other types: just check if equal
     else:
         if v1 != v2:
             r = False
             if verb:
-                print "Key mismatch:", k, v1, v2
+                print "Key mismatch:", v1, v2
 
     return r
 
-def isdicsimilar(dic1, dic2, verb=False, dtol=dtol):
+
+def isdicsimilar(dic1, dic2, verb=False, dtol=DTOL):
     """
     Compare two dictionaries for differences
 
-    Float and int types compared within dtol. Lists and dictionaries are 
+    Float and int types compared within dtol. Lists and dictionaries are
     checked recursively all other checked by simple equivalence
 
     Parameters
@@ -137,19 +140,19 @@ def isdicsimilar(dic1, dic2, verb=False, dtol=dtol):
     verb : bool, optional
         Set True for verbose reporting.
     dtol : float, optional
-        Maximum allowable difference between int and float elements if dic1 
+        Maximum allowable difference between int and float elements if dic1
         and dic2.
 
     Returns
     -------
     r1 : bool
         True is dic1 and dic2 are similar, False if they differ.
- 
+
     """
     # create copies of the two dictionaries
     dic1 = dict(dic1)
     dic2 = dict(dic2)
-    
+
     # set return value to True
     r = True
 
@@ -160,7 +163,7 @@ def isdicsimilar(dic1, dic2, verb=False, dtol=dtol):
     iset = set.intersection(kset1, kset2)
 
     # print out any keys not in both dictionaries
-    if len(dset) !=0:
+    if len(dset) != 0:
         r = False
         if verb:
             print "Keys not in both dictionaries:", dset
@@ -170,17 +173,19 @@ def isdicsimilar(dic1, dic2, verb=False, dtol=dtol):
         v1, v2 = dic1[k], dic2[k]
 
         if not isitemsimilar(v1, v2, verb=verb, dtol=dtol):
+            print "For key:", k
             r = False
 
     return r
 
-def islistsimilar(l1, l2, verb=False, dtol=dtol):
+
+def islistsimilar(l1, l2, verb=False, dtol=DTOL):
     """
     Compare two lists (or iterable) for differences
 
     See :py:func:`isdicsimilar` for Parameters.
 
-    """ 
+    """
     # set return value to True
     r = True
 
