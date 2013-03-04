2fae06f3f49b7cdef0e6cbddecd6cbd5ac266007
Jonathan Helmus <jhelmus@carbon.(none)>
2013-03-03 22:31:23 -0500

    Minor doc changes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'nmrglue'
-copyright = u'2010-2012, Jonathan J. Helmus'
+copyright = u'2010-2013, Jonathan J. Helmus'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
