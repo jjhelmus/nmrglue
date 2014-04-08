304e3b8c9f7892bf928d5b1c5a1ff2146440ce52
Timothy J. Ragan <tragan@nimr.mrc.ac.uk>
2014-04-08 15:41:56 +0100

    Added test for pipe_proc.tp logic.  Original change now fails these tests without the added logic. Added an option to ignore differences between the min, max, display min and display max values in the headers for tests, then use this option for the tp tests.

--- tests/pipe_proc_tests/tp.py
+++ tests/pipe_proc_tests/tp.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 """ Create files for tp unit test """
 
+from subprocess import check_output
+
 import nmrglue.fileio.pipe as pipe
 import nmrglue.process.pipe_proc as p
 
@@ -15,3 +17,36 @@ pipe.write("tp2.glue", d, a, overwrite=True)
 d, a = pipe.read("time_complex.fid")
 d, a = p.tp(d, a, nohyper=True)
 pipe.write("tp3.glue", d, a, overwrite=True)
+
+
+pipe_command = """\
+nmrPipe -in ./time_complex.fid                \
+| nmrPipe  -fn FT  -auto \
+-ov -out time-freq.c.ft1"""
+check_output(pipe_command, shell=True)
+
+d, a = pipe.read("time-freq.c.ft1")
+d, a = p.tp(d, a, hyper=True)
+pipe.write("tp4.glue", d, a, overwrite=True)
+
+d, a = pipe.read("time-freq.c.ft1")
+d, a = p.tp(d, a, auto=True)
+pipe.write("tp5.glue", d, a, overwrite=True)
+
+os.remove("time-freq.c.ft1")
+
+pipe_command = """\
+nmrPipe -in ./time_complex.fid                \
+| nmrPipe  -fn FT  -auto -di \
+-ov -out time-freq.r.ft1"""
+check_output(pipe_command, shell=True)
+
+d, a = pipe.read("time-freq.r.ft1")
+d, a = p.tp(d, a)
+pipe.write("tp6.glue", d, a, overwrite=True)
+
+d, a = pipe.read("time-freq.r.ft1")
+d, a = p.tp(d, a, auto=True)
+pipe.write("tp7.glue", d, a, overwrite=True)
+
+os.remove("time-freq.r.ft1")
