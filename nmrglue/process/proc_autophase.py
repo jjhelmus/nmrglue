1d6d25b267190d9e5dee90c5bd4bca1cd80f07b7
Jonathan Helmus <jjhelmus@gmail.com>
2015-06-08 12:52:30 -0500

    MAINT: Whitespace cleanup of proc_autophase module

--- nmrglue/process/proc_autophase.py
+++ nmrglue/process/proc_autophase.py
@@ -1,6 +1,6 @@
 """
 Automated phase correction
-These functions provide support for automatic phasing of NMR data. They 
+These functions provide support for automatic phasing of NMR data. They
 consist of the core `autops` function which performs the optimisation and
 a set of private functions for calculating a spectral phase quality score
 for a provided spectrum.
@@ -10,6 +10,7 @@ import numpy as np
 import scipy.optimize
 
 from .proc_base import ps
+
 
 def autops(data, fn, p0=0.0, p1=0.0):
     """
@@ -38,17 +39,18 @@ def autops(data, fn, p0=0.0, p1=0.0):
             'peak_minima': _ps_peak_minima_score,
             'acme': _ps_acme_score,
         }[fn]
-    
+
     opt = [p0, p1]
     opt = scipy.optimize.fmin(fn, x0=opt, args=(data, ))
-    
-    phasedspc = ps(data, p0=opt[0], p1=opt[1])    
+
+    phasedspc = ps(data, p0=opt[0], p1=opt[1])
 
     return phasedspc
 
+
 def _ps_acme_score(ph, data):
     """
-    Phase correction using ACME algorithm by Chen Li et al. 
+    Phase correction using ACME algorithm by Chen Li et al.
     Journal of Magnetic Resonance 158 (2002) 164-168
 
     Parameters
@@ -63,7 +65,7 @@ def _ps_acme_score(ph, data):
     score : float
         Value of the objective function (phase score)
 
-    """        
+    """
     stepsize = 1
 
     phc0, phc1 = ph
@@ -90,7 +92,7 @@ def _ps_acme_score(ph, data):
         pfun = pfun + np.sum((as_/2) ** 2)
 
     p = 1000 * pfun
-    
+
     return h1s + p
 
 
@@ -98,10 +100,10 @@ def _ps_peak_minima_score(ph, data):
     """
     Phase correction using simple minima-minimisation around highest peak
 
-    This is a naive approach but is quick and often achieves reasonable 
-    results.  The optimisation is performed by finding the highest peak in the 
+    This is a naive approach but is quick and often achieves reasonable
+    results.  The optimisation is performed by finding the highest peak in the
     spectra (e.g. TMSP) and then attempting to reduce minima surrounding it.
-    
+
     Parameters
     ----------
     pd : tuple
@@ -114,7 +116,7 @@ def _ps_peak_minima_score(ph, data):
     score : float
         Value of the objective function (phase score)
 
-    """ 
+    """
 
     phc0, phc1 = ph
 
@@ -126,4 +128,3 @@ def _ps_peak_minima_score(ph, data):
     minb = np.min(data[i:i+100])
 
     return np.abs(mina - minb)
-
