f7cfdc5feeed154e7d7282f3704c2f014d78392e
Keith Fritzsching <kfritzsc@brandeis.edu>
2016-02-15 16:21:09 -0500

    Change fit and rev  to only  fft_possitive for bruker example.

--- examples/bruker_processed_1d/bruker_processed_1d.py
+++ examples/bruker_processed_1d/bruker_processed_1d.py
@@ -19,17 +19,16 @@ ppm_scale = uc.ppm_scale()
 # From FID
 dic1, data1 = ng.bruker.read(data_dir)
 
-# remove the digital filter, this data if from an analog spectrometer.
+# remove the digital filter, this data is from an analog spectrometer.
 # data = ng.bruker.remove_digital_filter(dic, data)
 
 # process the spectrum
 data1 = ng.proc_base.ls(data1, 1)             # left shift
 data1 = ng.proc_base.gm(data1, g2=1/2.8e3)    # To match proc data...
 data1 = ng.proc_base.zf_size(data1, 1024*32)  # zero fill
-data1 = ng.proc_base.fft(data1)               # FT
+data1 = ng.proc_base.fft_positive(data1)      # FT
 data1 = ng.proc_base.ps(data1, p0=93)         # phase is 180 off Bruker
 data1 = ng.proc_base.di(data1)                # discard
-data1 = ng.proc_base.rev(data1)               # reverse the data
 
 udic1 = ng.bruker.guess_udic(dic1, data1)
 uc1 = ng.fileiobase.uc_from_udic(udic1)
